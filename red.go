56ca803c031fe4a4c30d2d144913e77c0404beb9
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2022-10-16 23:48:24 +0900

    hoge

--- red.go
+++ red.go
@@ -1,5 +1,13 @@
 package main
-import ("fmt"; "encoding/json"; "time")
+import ("fmt"; "encoding/json"; "time"; "net/http")
+
+type handler struct {
+    n  int
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "testtest")
+}
 
 func execute (nodeId string) {
     var flowJson = []byte(`[{"id":"9677e77d09b17122","type":"inject","z":"979ccb452915f125","name":"","props":[{"p":"payload"},{"p":"topic","vt":"str"}],"repeat":"1","crontab":"","once":false,"onceDelay":0.1,"topic":"","payload":"","payloadType":"date","x":240,"y":160,"wires":[["3552c8ecdf006278"]]},{"id":"3552c8ecdf006278","type":"debug","z":"979ccb452915f125","name":"debug 1","active":true,"tosidebar":true,"console":true,"tostatus":false,"complete":"payload","targetType":"msg","statusVal":"","statusType":"auto","x":450,"y":160,"wires":[]}]`)
@@ -40,5 +48,6 @@ func execute (nodeId string) {
 
 func main () {
     execute("")
-    time.Sleep(24 * time.Hour)
+    http.Handle("/", new(handler))
+    http.ListenAndServe(":8080", nil)
 }
