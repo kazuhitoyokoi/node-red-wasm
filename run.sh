b6bdebe7cd0fd5e24c800110683d0dbb35fa5738
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2022-10-18 01:14:04 +0900

    asdf

--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@ cat $1 >> flows.go
 echo "\`}" >> flows.go
 GOOS=js GOARCH=wasm go build -o docs/red.wasm
 go run red.go flows.go
-rm flows.go
 python3 -m http.server 8080 &
 open http://localhost:8080/docs
