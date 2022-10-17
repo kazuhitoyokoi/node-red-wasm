97c4660608958346f7843bdb7dd2fefdf844e4b1
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2022-10-17 21:44:56 +0900

    hoge

--- red.go
+++ red.go
@@ -2,7 +2,7 @@ package main
 import ("fmt"; "encoding/json"; "time"; "net/http")
 
 type handler struct {
-    n  int
+    n int
 }
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +10,7 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func execute (nodeId string) {
-    var flowJson = []byte(`[{"id":"9677e77d09b17122","type":"inject","z":"979ccb452915f125","name":"","props":[{"p":"payload"},{"p":"topic","vt":"str"}],"repeat":"1","crontab":"","once":false,"onceDelay":0.1,"topic":"","payload":"","payloadType":"date","x":240,"y":160,"wires":[["3552c8ecdf006278"]]},{"id":"3552c8ecdf006278","type":"debug","z":"979ccb452915f125","name":"debug 1","active":true,"tosidebar":true,"console":true,"tostatus":false,"complete":"payload","targetType":"msg","statusVal":"","statusType":"auto","x":450,"y":160,"wires":[]}]`)
+    var flowJson = []byte(flows())
     var flowItems []map[string]interface {}
     json.Unmarshal(flowJson, &flowItems)
     for i := 0; i < len(flowItems); i++ {
@@ -49,5 +49,5 @@ func execute (nodeId string) {
 func main () {
     execute("")
     http.Handle("/", new(handler))
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":1880", nil)
 }
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 GOOS=js GOARCH=wasm go build -o docs/red.wasm
-go run red.go
-python3 -m http.server 8080 &
-open http://localhost:8080
+go run red.go flows.go
+#python3 -m http.server 8080 &
+#open http://localhost:8080/docs
