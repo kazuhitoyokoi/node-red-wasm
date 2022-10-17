e0c6af2bc06bbe3ebf8e41f9c56c5443d4e77f15
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2022-10-18 01:09:08 +0900

    success to create shell

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
+echo "package main; func flows() string { return \`" > flows.go
+cat $1 >> flows.go
+echo "\`}" >> flows.go
 GOOS=js GOARCH=wasm go build -o docs/red.wasm
 go run red.go flows.go
+rm flows.go
 python3 -m http.server 8080 &
 open http://localhost:8080/docs
