6a181701f5fd7c7f87fc0e1a38e2de6f3bad7e6b
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2022-10-19 01:39:23 +0900

    hoge

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
-echo "package main; func flows() string { return \`" > flows.go
+#echo "package main; func flows() string { return \`" > flows.go
 #cat $1 >> flows.go
-#cat examples/inject_debug_flows.json >> flows.go
-cat examples/iss_flows.json >> flows.go
+#echo "\`}" >> flows.go
+
+echo "package main; func flows() string { return \`" > flows.go
+cat examples/console.json >> flows.go
 echo "\`}" >> flows.go
-GOOS=js GOARCH=wasm go build -o docs/red.wasm
-go run red.go flows.go
+GOOS=js GOARCH=wasm go build -o docs/console.wasm
+
+echo "package main; func flows() string { return \`" > flows.go
+cat examples/worldmap.json >> flows.go
+echo "\`}" >> flows.go
+GOOS=js GOARCH=wasm go build -o docs/worldmap.wasm
+
+#go run red.go flows.go
 python3 -m http.server 8080 &
 open http://localhost:8080/docs
